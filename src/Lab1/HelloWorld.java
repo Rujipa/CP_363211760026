39c526ac5283a26da9d91bec52d964a9644b6c1e
Rujipa <rujipa.p@rmutsvmail.com>
2020-07-15 14:56:15 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("HelloWorld !!!" );
+        System.out.println("Rujipa Pensawad" );
+        System.out.println("363211760026" );
+        System.out.println("เทคโนโลยีการจัดการ" );
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ" );
 
 
 
