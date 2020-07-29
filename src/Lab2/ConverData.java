b5c31e9c53840241171de05f28c577bd7526161b
rujipa <rujipa.p@rmtsvmail.com>
2020-07-29 15:55:58 +0700

    Data and Basic input

---
+++ src/Lab2/ConverData.java
@@ -0,0 +1,21 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class ConverData {
+    public static void main(String[] args) {
+
+        //สร้าง object ของคลาส scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How old are you ? : " );
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println(age);
+
+
+
+
+    }//main
+
+
+}//class
