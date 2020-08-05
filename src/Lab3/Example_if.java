615327c7e2e7253b22ec91ddf28bea16ec74118f
rujipa <rujipa.p@rmtsvmail.com>
2020-08-05 15:42:09 +0700

    Exercise ch 5

---
+++ src/Lab3/Example_if.java
@@ -0,0 +1,16 @@
+package Lab3;
+
+public class Example_if {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 30;
+
+        if (x < y) {
+            System.out.println("x more than y");
+            System.out.println("Heello.");
+        }
+        System.out.println("Good Bye.");
+
+    }
+}
