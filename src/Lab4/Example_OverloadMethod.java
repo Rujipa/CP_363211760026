e655d565261798a92f147e4a45bc65458ee4d27d
rujipa <rujipa.p@rmtsvmail.com>
2020-08-26 15:33:58 +0700

    Exercise ch 5

--- src/Lab4/Example_OverloadMethod.java
+++ src/Lab4/Example_OverloadMethod.java
@@ -8,10 +8,15 @@ public class Example_OverloadMethod {
         //else
         //  return y;
     }
-    public static double finMax(double x, double y){
+    public static double findMax(double x, double y){
         return (x>y)? x:y;
     }
     public static void main(String[] args) {
+        int max =findMax(5,10);
+        System.out.println("Maximum is"+max);
+        double max2 = findMax(5.0,10.0);
+        System.out.println("Maximum(2) is"+max2);
+
 
     }//main
 }
